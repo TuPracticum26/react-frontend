a91a0f7b4116d448a27a541c9636a156573ca200
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-05 11:12:30 +0300

    feat: make App the root

---
+++ src/App/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState, StrictMode } from "react";
+import { createRoot } from 'react-dom/client'
+import Header from "../Header/Header";
+import "./App.css";
+
+function Test() {
+    const [testMessage, SetTestMessage] = useState([]);
+    
+    useEffect(() => {
+        async function testApi() {
+            const response = await fetch("/api/v1/users");
+            const data = await response.json();
+            console.log(data);
+            SetTestMessage(data);
+            return data;
+        }
+        testApi();
+    }, []);
+    
+    useEffect(() => {
+        console.log(testMessage);
+    }, [testMessage]);
+
+    return (
+        <>
+            {testMessage.map((test, index) => (
+                <div
+                key={index}
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: '1rem'
+                }}
+                >
+                    <p>{test.id}</p>
+                    <p>{test.username}</p>
+                    <p>{test.role}</p>
+                </div>
+            ))}
+        </>
+    )
+
+}
+
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <Header />
+        <Test/>
+    </StrictMode>,
+);
