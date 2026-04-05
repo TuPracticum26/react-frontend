b501e7854f5e60352a03e6b603f0f5f1a28bf143
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-05 18:50:34 +0300

    feat: add TanStack Router

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/react-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   server: {
     proxy: {
       '/api': 'http://localhost:8080'
