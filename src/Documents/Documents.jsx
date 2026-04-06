551b3a7be10a13588316f0b5dd04303a228102fc
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-06 19:59:04 +0300

    feat: add an all documents page

--- src/Documents/Documents.jsx
+++ src/Documents/Documents.jsx
@@ -1,8 +1,31 @@
-import DocumentsStyles from './Documents.module.css'
+import DocumentsStyles from "./Documents.module.css";
+import useGetDocuments from "../hooks/useGetDocuments";
+import { Link } from "@tanstack/react-router";
+
 export default function Documents() {
+    const documents = useGetDocuments();
     return (
         <div className={DocumentsStyles.documents}>
-            <h1>Document</h1>
+            <h1>All Documents</h1>
+            <div className={DocumentsStyles["documents-container"]}>
+                {documents.map((document) => (
+                    <Link
+                        to={`/documents/${document.id}`}
+                        className="RouterLink"
+                        key={document.id}
+                    >
+                        <div className={DocumentsStyles["document-card"]}>
+                            <div className={DocumentsStyles["title-author"]}>
+                                <h4>{document.title}</h4>
+                                <div className={DocumentsStyles["author-date"]}>
+                                    <p>{document.authorUsername}</p>
+                                    <p>{document.creationDate.slice(0, 10)}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
