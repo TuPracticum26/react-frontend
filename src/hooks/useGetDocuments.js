d4873b5909750192a888319cf6030e28b60d208b
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-06 19:57:38 +0300

    feat: add a custom hooks folder

---
+++ src/hooks/useGetDocuments.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export default function useGetDocuments() {
+    const [documents, setDocument] = useState([]);
+
+    useEffect(() => {
+        async function testApi() {
+            const response = await fetch("/api/v1/documents");
+            const data = await response.json();
+            setDocument(data);
+            return data;
+        }
+        testApi();
+    }, [])
+
+    return documents;
+}
