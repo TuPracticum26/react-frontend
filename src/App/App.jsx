e78fabcc7ba010c0f35116e8594deb04bc736160
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-07 12:54:14 +0300

    rename: App.css -> global.css

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client'
-import "./App.css";
+import "./global.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from '../routeTree.gen'
 
