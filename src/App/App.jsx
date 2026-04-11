59c865b81ca46857ecf4470de1f05897b752d72b
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-11 22:28:20 +0300

    style: prettier

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import "./global.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from '../routeTree.gen'
+import { routeTree } from "../routeTree.gen";
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
     <StrictMode>
         <RouterProvider router={router} />
     </StrictMode>,
