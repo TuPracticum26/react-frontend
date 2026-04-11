d3e7c492cf4ebf2563d1f6187c642d8f49aae58f
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-11 22:37:41 +0300

    feat: show Home/Login/Register if no auth is provided

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,9 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useLocation } from "@tanstack/react-router";
 
-
-export default function Home() {
+export default function Home({ children }) {
+    const pathname = useLocation({
+        select: (location) => location.pathname,
+    });
     const buttonStyle = {
         width: "100%",
         padding: "12px",
@@ -14,28 +16,41 @@ export default function Home() {
     };
 
     return (
-        <div style={{
-            mineHeight: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center"
-        }}>
-            <h1>Welcome to the Document Manager App!</h1>
-            <p>Please log in to continue</p>
-            <Link to="/login" className="RouterLink">
-                <button
-                    style={buttonStyle}>
-                        Login
-                </button>
-            </Link>
-            <p style={{ fontStyle: "14px", color: "#555"}}>
-                You don't have an account yet?{" "}
-                <Link to="/register" style={{ color: "#4f64dc", textDecoration: "none", fontweight: "bold" }}>
-                    Register here.
-                </Link>
-            </p>
+        <div
+            style={{
+                mineHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+            }}
+        >
+            {pathname == "/login" || pathname == "/register" ? (
+                <>{children}</>
+            ) : (
+                <>
+                    {console.log(pathname)}
+                    <h1>Welcome to the Document Manager App!</h1>
+                    <p>Please log in to continue</p>
+                    <Link to="/login" className="RouterLink">
+                        <button style={buttonStyle}>Login</button>
+                    </Link>
+                    <p style={{ fontStyle: "14px", color: "#555" }}>
+                        You don't have an account yet?{" "}
+                        <Link
+                            to="/register"
+                            style={{
+                                color: "#4f64dc",
+                                textDecoration: "none",
+                                fontweight: "bold",
+                            }}
+                        >
+                            Register here.
+                        </Link>
+                    </p>
+                </>
+            )}
         </div>
-    )
+    );
 }
