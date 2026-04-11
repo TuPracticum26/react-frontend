a21e5e7011df1151060a8ca9070f3bc506ae5742
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-11 22:36:44 +0300

    feat: show components based on authentication

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
-import { createRootRoute, createRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import rootStyles from "./root.module.css";
+import Home from "../Home/Home";
+
+const token = localStorage.getItem("token");
 
 export const Route = createRootRoute({
     component: () => {
         return (
             <>
-                <Header />
-                <div className={rootStyles.body}>
-                    <Sidebar />
-                    <main>
+                {token ? (
+                    <>
+                        <Header />
+                        <div className={rootStyles.body}>
+                            <Sidebar />
+                            <main>
+                                <Outlet />
+                            </main>
+                        </div>
+                        <TanStackRouterDevtools />
+                    </>
+                ) : (
+                    <Home>
                         <Outlet />
-                    </main>
-                </div>
-                <TanStackRouterDevtools />
+                    </Home>
+                )}
             </>
         );
     },
