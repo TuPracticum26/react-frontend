4def064cb39fb39dd3f6d6135a8fd9ee4da12cd6
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-14 20:39:02 +0300

    feat: imporove Home screen when logged in

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,8 @@
 import { Link, useLocation } from "@tanstack/react-router";
+import DocumentImage from '../../public/Home_Document.png'
 
 export default function Home({ children }) {
+    const token = JSON.parse(localStorage.getItem("token"));
     const pathname = useLocation({
         select: (location) => location.pathname,
     });
@@ -18,21 +20,24 @@ export default function Home({ children }) {
     return (
         <div
             style={{
-                mineHeight: "100vh",
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "center",
                 alignItems: "center",
                 textAlign: "center",
-                width: "100%",
+                width: "80%",
                 height: "100%",
+                paddingLeft: "20%",
             }}
         >
-            {pathname == "/login" || pathname == "/register" ? (
+            <div style={{backgroundColor: "var(--hover-color)", padding: "2rem", height: "100%", width: "100%"}}>
+
+            
+            {!token ? 
+            pathname == "/login" || pathname == "/register" ? (
                 <>{children}</>
             ) : (
                 <>
-                    {console.log(pathname)}
                     <h1>Welcome to the Document Manager App!</h1>
                     <p>Please log in to continue</p>
                     <Link to="/login" className="RouterLink">
@@ -47,12 +52,22 @@ export default function Home({ children }) {
                                 textDecoration: "none",
                                 fontweight: "bold",
                             }}
-                        >
+                            >
                             Register here.
                         </Link>
                     </p>
                 </>
-            )}
-        </div>
-    );
+            )
+            :
+            <div style={{display: "flex", flexDirection: "column", gap:"2rem", justifyContent: "center", alignItems: "center"}}>
+            <h1 style={{color:"var(--logo-color)"}}>Welcome to the Document Manager App</h1>
+            <h2>Navigate through the menus to read/write/alter Documents!</h2>
+            <div style={{backgroundColor: "var(--secondary-color-soft)", borderRadius: "100vw", minWidth: "600px", minHeight: "600px", display: "flex", flexDirection: "column", gap:"2rem", justifyContent: "center", alignItems: "center"}}>
+                <img src={DocumentImage} alt="" style={{maxWidth: "400px"}}/>
+            </div>
+            </div> 
+            }
+            </div>
+            </div>
+        );
 }
