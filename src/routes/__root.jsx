172b91963f3929a9447ec9aa31f4ee6f809f9f8e
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-14 21:04:47 +0300

    feat: add not found page

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,11 +1,12 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet, useLocation } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import rootStyles from "./root.module.css";
 import Home from "../Home/Home";
 import { Theme } from "@radix-ui/themes";
+import NotFoundImage from "../../public/Not_Found.png";
 
 const token = localStorage.getItem("token");
 
@@ -33,6 +34,20 @@ export const Route = createRootRoute({
                 <TanStackRouterDevtools />
                 <ReactQueryDevtools />
             </>
+        );
+    },
+    notFoundComponent: () => {
+        const pathname = useLocation({
+            select: (location) => location.pathname,
+        });
+        return (
+            <div className={rootStyles["not-found-container"]}>
+                <h1>404</h1>
+                <p>{pathname}</p>
+                <h2>Provided link to page was not found!</h2>
+                <h4>Click <span>here</span> to go back to the home page!</h4>
+                <img src={NotFoundImage} alt="" />
+            </div>
         );
     },
 });
