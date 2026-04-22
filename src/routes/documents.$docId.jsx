48d80d7c009d0b59ffdec8cf5311b7b23cdd88ee
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-04-23 00:20:42 +0300

    style: reorder documents routes

--- src/routes/documents.$docId.jsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import DocumentView from '../Documents/DocumentView/DocumentView';
-
-export const Route = createFileRoute('/documents/$docId')({
-  component: DocumentView,
-})
